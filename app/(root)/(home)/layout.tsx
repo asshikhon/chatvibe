f59d06836cefcb886b7bdaa43947fdb83811aeee
asshikhon <asshikhon@gmail.com>
2024-09-26 05:40:43 +0600

    added new meeting functionality

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,16 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+
+export const metadata: Metadata = {
+  title: "ChatVibe",
+  description: "video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const HomeLayout = ({children}: {children: ReactNode} ) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,15 @@
 import StreamVideoProvider from '@/provider/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+
+export const metadata: Metadata = {
+  title: "ChatVibe",
+  description: "video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({children}: {children: ReactNode} ) => {
   return (
